099affddcaff24b48c2c0b85dfa9cb70b0a36299
scd7896 <scd1212@naver.com>
2022-04-04 22:33:35 +0900

    fix: Github page 배포

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = function () {
     },
     output: {
       filename: "index.js",
-      path: path.resolve(__dirname, "dist"),
+      path: path.resolve(__dirname, "example"),
     },
   };
 };
