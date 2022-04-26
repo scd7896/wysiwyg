141c291b6ffefc5c0963abb4640960e472343691
scd7896 <scd1212@naver.com>
2022-04-26 19:24:21 +0900

    fix: event추가 tab이벤트를 넣어서 list 자동 넣을 수 있도록 수정

--- src/components/Menu/menuFunction/list.ts
+++ src/components/Menu/menuFunction/list.ts
@@ -1,3 +1,4 @@
+import EventSingleton from "../../../event/EventSingleton";
 import { RangeSingleton } from "../../../model";
 import { findParentByNodeName } from "../../../utils/dom";
 
@@ -7,6 +8,33 @@ class List {
     button.textContent = "list";
     button.addEventListener("click", () => this.insertUList());
     parent.appendChild(button);
+    setTimeout(() => {
+      const board: HTMLDivElement = parent.parentElement.querySelector(".board");
+      EventSingleton.getInstance().on("tabkey", () => {
+        this.tabulList();
+      });
+
+      board.addEventListener("keydown", (e) => {
+        if (e.key === "Tab") {
+          e.preventDefault();
+          EventSingleton.getInstance().emit("tabkey");
+        }
+      });
+    }, 11);
+  }
+
+  tabulList() {
+    if (RangeSingleton.getInstance().type === "Range") return;
+    const parentLi = findParentByNodeName(RangeSingleton.getInstance().anchorNode as HTMLElement, "LI");
+    if (parentLi) {
+      const ul = document.createElement("ul");
+      const li = document.createElement("li");
+      li.textContent = " ";
+      ul.appendChild(li);
+      li.innerHTML = parentLi.innerHTML;
+      parentLi.parentElement.replaceChild(ul, parentLi);
+      RangeSingleton.getInstance().changeFocusNode(li);
+    }
   }
 
   insertUList() {
