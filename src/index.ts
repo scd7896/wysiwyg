4129ebde0d638a0790f306e8872dff17de74442a
scd7896 <scd1212@naver.com>
2022-05-09 21:27:13 +0900

    feat: WYSIWYG에 public Method 추가함

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { WriteBoard, Menu, Resizer } from "./components";
 import EventSingleton from "./event/EventSingleton";
 import { ImageResizerStore, RangeSingleton } from "./model";
+import HistoryStore from "./model/HistoryStore";
 import { findResizeNodeByParentNode, setStyle } from "./utils/dom";
 
 export class WYSIWYG {
@@ -26,4 +27,28 @@ export class WYSIWYG {
       ImageResizerStore.setInitlization();
     }
   };
+
+  insertNode(element: HTMLElement) {
+    RangeSingleton.getInstance().insertNodeAndFoucs(element);
+  }
+
+  setRangeStyle(style: Record<string, string>) {
+    RangeSingleton.getInstance().setStyle(style);
+  }
+
+  undo() {
+    return HistoryStore.undo();
+  }
+
+  redo() {
+    return HistoryStore.redo();
+  }
+
+  get undoHistory() {
+    return HistoryStore.undoHistory;
+  }
+
+  get redoHistory() {
+    return HistoryStore.redoHistory;
+  }
 }
