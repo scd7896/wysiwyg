19eb49ae22cdea2c05cb07779b4e3b067c63c5a2
scd7896 <scd1212@naver.com>
2022-05-15 21:40:53 +0900

    fix: test code 수정

--- src/model/HistoryStore.test.ts
+++ src/model/HistoryStore.test.ts
@@ -2,10 +2,11 @@ import HistoryStore from "./HistoryStore";
 const firstArray = ["abcdefg", "hijklmnok", "123456"];
 const secondArray = ["abcdefg", "123456"];
 const thirdArray = ["abcdefg", "123456", "abcdefg"];
+const historyStore = new HistoryStore();
 
 describe("HistorStore Test", () => {
   it("1차 string 추가 테스트", () => {
-    const result = HistoryStore.setNextChild(firstArray);
+    const result = historyStore.setNextChild(firstArray);
 
     expect(result.length).toBe(3);
     expect(result).toEqual([
@@ -13,39 +14,39 @@ describe("HistorStore Test", () => {
       { line: 1, type: "insert", value: firstArray[1] },
       { line: 2, type: "insert", value: firstArray[2] },
     ]);
-    expect(HistoryStore.currentChild).toEqual(firstArray);
+    expect(historyStore.currentChild).toEqual(firstArray);
   });
 
   it("2차 string 추가 테스트", () => {
-    const result = HistoryStore.setNextChild(secondArray);
+    const result = historyStore.setNextChild(secondArray);
 
     expect(result).toEqual([{ line: 1, type: "delete", value: firstArray[1] }]);
   });
 
   it("undo 테스트", () => {
-    HistoryStore.undo();
+    historyStore.undo();
 
-    expect(HistoryStore.currentChild).toEqual(firstArray);
+    expect(historyStore.currentChild).toEqual(firstArray);
   });
 
   it("redo 테스트", () => {
-    HistoryStore.redo();
-    expect(HistoryStore.currentChild).toEqual(secondArray);
+    historyStore.redo();
+    expect(historyStore.currentChild).toEqual(secondArray);
   });
 
   it("3차 추가", () => {
-    const result = HistoryStore.setNextChild(thirdArray);
+    const result = historyStore.setNextChild(thirdArray);
 
     expect(result).toEqual([{ line: 2, type: "insert", value: thirdArray[2] }]);
   });
 
   it("undo 테스트", () => {
-    HistoryStore.undo();
-    expect(HistoryStore.currentChild).toEqual(secondArray);
+    historyStore.undo();
+    expect(historyStore.currentChild).toEqual(secondArray);
   });
 
   it("redu 테스트", () => {
-    HistoryStore.redo();
-    expect(HistoryStore.currentChild).toEqual(thirdArray);
+    historyStore.redo();
+    expect(historyStore.currentChild).toEqual(thirdArray);
   });
 });
